86d11431af395c3d6ca6e24c7c8f1a9f7408e0ab
dhavalkanjariya2803 <dhavalkanjariya2803@gmail.com>
2022-07-27 01:29:36 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- <Tutorial/> -->
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  // async asyncData({ params, $axios }) {
+  //   const post = await $axios.$get(`https://jsonplaceholder.typicode.com/users`)
+  //   console.log(post, 'datata')
+  //   return { }
+  // }
+}
+</script>
